2224560a46d9dd124e1d7fd070de21c14659f3cb
vedmborade <67070121+vedmborade@users.noreply.github.com>
2024-03-11 01:08:30 -0400

    Create HealthAPP2.swift

---
+++ HealthAPP2.swift
@@ -0,0 +1,34 @@
+//
+//  HealthAPP2App.swift
+//  HealthAPP2
+//
+//  Created by Ved Borade on 3/8/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+@main
+struct HealthAPP2App: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+
